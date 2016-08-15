89ba0d1d0230af63c928ba1f6fe57deb13be6e36
chengkj99 <ckj121@126.com>
2016-08-15 11:24:31 +0800

    test

--- public/javascripts/es5/file_upload.js
+++ public/javascripts/es5/file_upload.js
@@ -1,7 +1,7 @@
 
-import { square, diag } from './lib';
-console.log(square(11)); // 121
-console.log(diag(4, 3));
+ import square from 'lib';
+// console.log(square(11)); // 121
+// console.log(diag(4, 3));
 
 // import $ from 'jquery';
 // $(function () {
